2c6003ce1000850406f06e2e494f2a081827bd02
Bryan Asiimwe <a.brizzy.ba@gmail.com>
2023-09-19 00:28:58 +0300

    First commit

---
+++ Animal.js
@@ -0,0 +1,30 @@
+
+class Animal {
+  constructor(species, sound) {
+    this.species = species;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} says: ${this.sound}`);
+  }
+}
+
+
+class Dog extends Animal {
+  constructor(species, sound, color) {
+    super(species, sound);
+    this.color = color;
+  }
+
+
+  makeSound() {
+    console.log(`The ${this.color} dog says: ${this.sound}`);
+  }
+}
+
+
+const myDog = new Dog('Dog', 'Woof', 'Brown');
+
+
+myDog.makeSound();
